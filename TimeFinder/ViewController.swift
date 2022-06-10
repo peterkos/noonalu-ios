45bc91c9cd2d4dd11d65ffbface39633fd6791da
Peter Kos <pkos91@icloud.com>
2022-06-09 22:29:30 -0400

    Oops, lets use storyboards

---
+++ TimeFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeFinder
+//
+//  Created by Peter Kos on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
