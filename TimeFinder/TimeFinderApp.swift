bd4faa7c114cd21d185c51be56cf080a3303068b
Peter Kos <pkos91@icloud.com>
2022-06-09 22:13:39 -0400

    Hello world

---
+++ TimeFinder/TimeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFinderApp.swift
+//  TimeFinder
+//
+//  Created by Peter Kos on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
